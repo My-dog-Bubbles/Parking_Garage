7abceba2897f288725d27f7b1134c678de8cdffc
My-dog-Bubbles <amcarmichael2@my.waketech.edu>
2026-04-23 08:06:43 -0400

    import file

--- test_garage.py
+++ test_garage.py
@@ -1,7 +1,14 @@
-from garage import exit_garage
+from garage import enter_garage
 
 def test_enter_garage_car_to_long(garage):
     garage["cars"] = {"ca","cb", "cc", "cd", "ce","cf", "cg","vksjd","nknjd","kjsdn","ksdnlsnd"}
     with pytest.raises(ValueError):
         enter_garage(garage, 5, "2pm")
 
+# def test_enter_garage_car_to_long():
+#     with pytest.raise(ValueError)
+#         enter_garage(garage, 5, "2pm")
+
+# def test_exit_garage_wrong_ID():
+#     with pytest.raise(KeyError)
+#         exit_garage(garage, 3)
