0f37caf8de6912b5303779b40cd738929b59fa95
My-dog-Bubbles <amcarmichael2@my.waketech.edu>
2026-04-23 08:03:17 -0400

    fix test

--- test_garage.py
+++ test_garage.py
@@ -2,6 +2,6 @@ from garage import exit_garage
 
 def test_enter_garage_car_to_long():
     garage["cars"] = {"ca","cb", "cc", "cd", "ce","cf", "cg","vksjd","nknjd","kjsdn","ksdnlsnd"}
-    with pytest.raises(ValueError)
+    with pytest.raises(ValueError):
         enter_garage(garage, 5, "2pm")
 
