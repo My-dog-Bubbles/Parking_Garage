11110ec4bdcd8e43880b8a075fbbe0171c388910
My-dog-Bubbles <amcarmichael2@my.waketech.edu>
2026-04-23 08:09:46 -0400

    made test for neg hrs or rate

--- test_garage.py
+++ test_garage.py
@@ -1,10 +1,10 @@
 import pytest
-from garage import enter_garage
+from garage import calculate_fee
 
-def test_enter_garage_car_to_long(garage):
-    garage["cars"] = {"ca","cb", "cc", "cd", "ce","cf", "cg","vksjd","nknjd","kjsdn","ksdnlsnd"}
-    with pytest.raises(ValueError):
-        enter_garage(garage, 5, "2pm")
+# def test_enter_garage_car_to_long(garage):
+#     garage["cars"] = {"ca","cb", "cc", "cd", "ce","cf", "cg","vksjd","nknjd","kjsdn","ksdnlsnd"}
+#     with pytest.raises(ValueError):
+#         enter_garage(garage, 5, "2pm")
 
 # def test_enter_garage_car_to_long():
 #     with pytest.raise(ValueError)
@@ -13,3 +13,7 @@ def test_enter_garage_car_to_long(garage):
 # def test_exit_garage_wrong_ID():
 #     with pytest.raise(KeyError)
 #         exit_garage(garage, 3)
+
+def test_calculate_fee_hours_or_rate_neg():
+    with pytest.raises(ValueError):
+        calculate_fee(-2,2)
